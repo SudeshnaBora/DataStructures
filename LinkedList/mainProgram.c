bd189ffe70042f3494ae3326b19a5677bec6ee93
SudeshnaBora <sudeshnamani1@gmail.com>
2015-09-06 22:03:03 +0530

    driver program

--- LinkedList/mainProgram.c
+++ LinkedList/mainProgram.c
@@ -3,13 +3,23 @@
 
 int main()
 {
-    creation();
+    int data,choice=1;
+    LinkedListCreation();
     //creates a linked list
     printf("\n");
     //display the list created only if the list exists
     if(head!=0)
     {
-       display();
+       LinkedListDisplay();
     }
+    while(choice!=0)
+    {
+        printf("Enter the value you want to delete\n");
+        scanf("%d",&data);
+        LinkedListDeletion(data);
+        printf("Do you want to continue 0:no\n");
+        scanf("%d",&choice);
+    }
+    display();
     return 0;
 }
